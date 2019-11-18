ad1c086d65fa2ecb89bc8e85ac42900c3e86b9ad
kellmant <kellman@meghu.ca>
2019-11-18 18:55:41 -0500

    testing new parse

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ async function parseObjectUse(objdat) {
 		Object.keys(objdat).forEach(uid => {
 			//myres = myres.concat(get([uid, '0', 'used-directly', '0', 'objects'], usedobj[ip][uid]))
 			myres['objects'] = myres['objects'].concat(get([uid, '0', 'used-directly', '0', 'objects'], objdat))
-			myres['access'] = myres['objects'].concat(get([uid, '0', 'used-directly', '0', 'access-control-rules'], objdat))
+			myres['access'] = myres['objects'].concat(get([uid, '0', 'used-directly', '0', 'access-control-rules', '0'], objdat))
 			//myres = myres.concat(objdat)
 		});
 		//let unique = [...new Set(myres)]
