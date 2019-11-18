c7e28b1807cbdcdfe5b5f79c13e1ec334f6f49ae
kellmant <kellman@meghu.ca>
2019-11-18 17:37:22 -0500

    testing new parse

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ async function parseObjectUse() {
 	try {
 		var myres = []
 		Object.keys(usedobj[ip]).forEach(uid => {
-			myres = myres.concat(get([uid, '0', 'used-directly', '0', 'objects'], usedobj[ip][uid]))
+			//myres = myres.concat(get([uid, '0', 'used-directly', '0', 'objects'], usedobj[ip][uid]))
 			myres = myres.concat(get([uid], usedobj[ip][uid]))
 			//myres = myres.concat(usedobj[ip][uid])
 		});
