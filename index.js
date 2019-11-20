c742162ec25081c63fe3ebb190900b8c2f035671
kellmant <kellman@meghu.ca>
2019-11-19 23:34:36 -0500

    add garbage collection

--- index.js
+++ index.js
@@ -553,7 +553,7 @@ async function doParse(objdat) {
 								let mycnt = Object.keys(objdat[ip][uid][usetype][used][arrs]).length
 								//console.log(Object.keys(objdat[ip][uid][usetype][used][arrs]))
 								//console.log(objdat[ip][uid][usetype][used][arrs])
-								console.log(mycnt + ' ' + arrs + ' ' + uid + ' used: ' + used)
+								console.log(mycnt + ' ' + arrs + ' ' + usetype + ' used: ' + used)
 								//allobjs[uid]
 								myarrs[arrs] = myarrs[arrs].concat(objdat[ip][uid][usetype][used][arrs])
 								myres[used] = myres[used].concat(myarrs)
