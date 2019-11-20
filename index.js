a98a82ce6039a46bcdbfe2362ace3e5c90361f0d
kellmant <kellman@meghu.ca>
2019-11-20 05:48:51 -0500

    new parser

--- index.js
+++ index.js
@@ -379,7 +379,7 @@ async function parseRuleUse(objdat) {
 		//		myret = myret.concat(myres[x])
 		//}
 		for (var x of allobjs[myrules]) {
-			console.log(x)
+			console.log(x.uid)
 		}
 		return allobjs
 	} catch (err) {
