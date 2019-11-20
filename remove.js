296c8ed4bec08d519e87d754e599b307fbd10864
kellmant <kellman@meghu.ca>
2019-11-20 08:22:34 -0500

    remove process

--- remove.js
+++ remove.js
@@ -51,6 +51,187 @@ if (objdata.garbage.length > 0) {
     console.log(objdata.garbage)
     console.log('Cleanup needed of object before removal')
 }
-Object.values(objdata.group).forEach(grp => {
-    console.log(grp)
-});
+
+main()
+
+async function myGroups() {
+    try {
+        let mycmd = 'set-group'
+        for (var x of objdata.group) {
+            console.log(mycmd)
+            console.log(x)
+        }
+        return 
+    } catch (err) {
+        console.log('Error in myGroups : ' + err)
+    }
+}
+
+
+async function main() {
+	startSession(mycred)
+		.then(sessiontoken => setSession(sessiontoken))
+		.then(() => myGroups())
+		.then(() => endSession())
+		.then(exitstat => console.log(exitstat))
+		//.then(() => console.dir(cleanobj))
+		//.then(thindat => console.log(thindat))
+	.catch(endSession)
+}
+
+async function setObject(myobj, mycmd) {
+	try {
+        var setit = toApi.doPost(myobj, mycmd)
+        let indat = await callOut(setit.options, setit.postData)
+		return indat
+	} catch (err) {
+		console.log('error in setObject : ' + err)
+	}
+}
+
+/**
+ * Create an authenticated session with the Check Point API
+ * @function startSession 
+ * @param {json} myauth Credentials used for API access
+ * @return {Object} The prepared session handler
+ */
+async function startSession(myauth) {
+    try {
+            console.log('starting session')
+            var setit = toApi.doPost(myauth, 'login')
+    //toApi.showOpt()
+            sessionid = await callOut(setit.options, setit.postData)
+            return sessionid
+    } catch (err) {
+            console.log('error in startSession')
+            console.log(err)
+    }
+}
+
+// set session token to header
+/**
+* Set the session handler for a Check Point API connection
+* @function setSession 
+* @param {Object} mysession A Check Point API session handler
+*/
+async function setSession(mysession) {
+    try {
+            console.log('setting session')
+            toApi.setToken(mysession)
+            //toApi.showOpt()
+            return
+    } catch (err) {
+            console.log('error in setSession')
+            console.log(err)
+    }
+}
+
+/**
+* Publish data to the Check Point API via a callout to HTTP POST
+* @function pubSession 
+* @return {Object} mysession A Check Point API session handler
+*/
+async function pubSession() {
+    try {
+            console.log('publishing session')
+    var mycmd = 'publish'
+    var nodata = {}
+            var mysession = await callOut(toApi.doPost(nodata, mycmd).options, toApi.doPost(nodata, mycmd).postData)
+               //toApi.showOpt()
+    await sleep(3000)
+            return mysession
+    } catch (err) {
+            console.log('error in pubSession : ' + err)
+    }
+}
+
+
+// end session and expire token from header
+/**
+* Safely logout from the Check Point API
+* @function endSession 
+* @return {Object} The completed Check Point API session handler
+*/
+async function endSession() {
+    try {
+            console.log('ending session')
+    var nodata = {}
+            var nosession = await callOut(toApi.doPost(nodata, 'logout').options, toApi.doPost(nodata, 'logout').postData)
+               //toApi.showOpt()
+            return nosession
+    } catch (err) {
+            console.log('error in endSession : ' + err)
+    }
+}
+
+// go get the rest api data
+/**
+* 
+* @param {json} options 
+* @param {*} postData 
+*/
+async function callOut(options, postData) {
+return new Promise((resolve, reject) => {
+        var req = https.request(options, (res) => {
+        var myret = ''
+                if (res.statusCode > 200) {
+                process.stdout.write(res.statusCode + ' : ' + res.statusMessage + ' ' + options.path);
+                }
+                res.on('data', (d) => {
+                        myret += d
+                });
+                res.on('end', () => {
+                        resolve(JSON.parse(myret))
+                });
+        });
+        req.on('error', (e) => {
+                reject(e);
+        });
+        if (postData) {
+                req.write(postData);
+        }
+        req.end();
+})
+}
+
+// save api output as json data to file
+/**
+* @function writeJson
+* @param {json} content 
+*/
+async function writeJson (content) {
+    try {
+            var newfile = myfilename + '.json'
+    console.log('writing file . . . ' + newfile)
+    console.log(typeof content)
+            const data = await fs.writeFileSync(newfile, JSON.stringify(content, undefined, 2))
+            //file written successfully
+    console.log(content)
+            console.log('Json data written to ' + newfile)
+            console.log('  --  ')
+            return content
+    } catch (err) {
+            console.error(err)
+    }
+}
+
+// easy way to wait
+/**
+* Promise'd sleep function to account for API round trip delays
+* @function sleep 
+* @param {int} ms Number of milliseconds to sleep  by
+* @return {Object} The completed promise after x time has passed
+*/
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+* the number of keys in use for a given object
+* @function Count 
+* @param {Object} obj The object to be checked
+* @return {int} The number of keys in use
+*/
+function countOf(obj) {
+return Object.keys(obj).length
+}
