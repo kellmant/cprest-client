d414530b629d445484f9b5de67f66c01dd4f8af8
kellmant <kellman@meghu.ca>
2019-11-21 19:16:29 -0500

    cleanup documentation

--- index.js
+++ index.js
@@ -150,6 +150,7 @@ async function admins() {
 
 /**
  * Create an authenticated session with the Check Point API
+ * @constructor
  * @param {mycred} credentials Credentials used for API access
  * @return {sessionid} The prepared session handler
  */
