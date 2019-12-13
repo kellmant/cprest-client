9415f0472ca77a7313b2ce5b13cbec5f0df1efae
kellmant <kellman@meghu.ca>
2019-12-13 18:17:01 -0500

    cleanup docs with functions

--- index.js
+++ index.js
@@ -651,7 +651,7 @@ async function pubSession() {
 */
 async function endSession() {
         try {
-                console.log('run function ' + myFunction.name)
+                console.log('ending api session')
 		var nodata = {}
                 var nosession = await callOut(toApi.doPost(nodata, 'logout').options, toApi.doPost(nodata, 'logout').postData)
                	//toApi.showOpt()
