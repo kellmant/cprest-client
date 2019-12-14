c9c42ff936a972045365f062dc1e487b2892799d
kellmant <kellman@meghu.ca>
2019-12-13 20:00:48 -0500

    cleanup docs with functions

--- remove.js
+++ remove.js
@@ -196,13 +196,13 @@ async function garbagecollection(trash) {
                 console.log(mytype)
                 if (mytype == 'nat-rule') {
                         //groupNat = groupBy(trash.mytype, 'package')
-                        console.log(mytype, trash.mytype)
+                        console.log(mytype, groupTypes.mytype)
                 }
                 //groupTypes.mytype = groupBy(groupTypes.mytype, )
                 //const groupRules = groupBy(groupTypes['nat-rule'], 'package')
         });
         Object.keys(groupNat).forEach(pkg => {
-                console.log(pkg, groupNat[pkg])
+                console.log('out of loop ' + pkg, groupNat[pkg])
         });
         /**
                 if (item.name) {
