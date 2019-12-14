667194263404dfb88dab923c44dca0da9f0a3f6c
kellmant <kellman@meghu.ca>
2019-12-14 05:34:14 -0500

    cleanup docs with functions

--- remove.js
+++ remove.js
@@ -390,7 +390,7 @@ return new Promise((resolve, reject) => {
         var req = https.request(options, (res) => {
         var myret = ''
                 if (res.statusCode > 200) {
-                        sessionstat.description += options.path + '=>' + res.statusCode + ':' + res.errorMessage + ' * ' 
+                        sessionstat.description += options.path + '=>' + res.statusCode + ':' + res.statusMessage + ' * ' 
                         //sessionstat.description += myret
                 } else {
                 //process.stdout.write(res.statusCode + ' : ' + res.statusMessage + ' ' + options.path);
