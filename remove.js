6a05846a8138c7529ee3b399ddb5e0275e323fd8
kellmant <kellman@meghu.ca>
2019-12-14 05:29:26 -0500

    cleanup docs with functions

--- remove.js
+++ remove.js
@@ -391,7 +391,7 @@ return new Promise((resolve, reject) => {
         var myret = ''
                 if (res.statusCode > 200) {
                         //sessionstat.description += options.path + '=>' + res.statusCode + ':' + res + ' * ' 
-                        sessionstat.description += JSON.parse(res)
+                        sessionstat.description += res
                 } else {
                 //process.stdout.write(res.statusCode + ' : ' + res.statusMessage + ' ' + options.path);
                 sessionstat.description += options.path + '=>' + res.statusCode + ':' + res.statusMessage + ' * ' 
