73c5c47085837df904ddf974ecd0ab67a920c729
kellmant <kellman@meghu.ca>
2019-12-15 02:30:32 -0500

    module build and cleanup

--- fun.js
+++ fun.js
@@ -162,19 +162,18 @@ function testme() {
     console.log('testme responded')
 }
 
-function groupBy(list, keyGetter) {
-    const map = new Map();
-    list.forEach((item) => {
-         const key = keyGetter(item);
-         const collection = map.get(key);
-         if (!collection) {
-             map.set(key, [item]);
-         } else {
-             collection.push(item);
-         }
-    });
-    return map;
-}
+// Accepts the array and key
+const groupBy = (array, key) => {
+    // Return the end result
+    return array.reduce((result, currentValue) => {
+      // If an array already present for key, push it to the array. Else create an array and push the object
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+      return result;
+    }, {}); // empty object is the initial value for result object
+};
 
 // easy way to wait
 function sleep(ms) {
