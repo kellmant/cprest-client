24511037332cbc60b835e4a0c4af7c40e7464dc8
kellmant <kellman@meghu.ca>
2019-12-18 19:50:41 -0500

    module build and cleanup

--- getrules.js
+++ getrules.js
@@ -90,7 +90,7 @@ async function getPackages() {
                         objarr = objarr.concat(objdata.packages)
                 }
         }
-        return objarr[0]
+        return { ...[objarr] }
     } catch (err) {
         console.log('error in showPackages : ' + err)
         }
