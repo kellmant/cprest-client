503f82c710e58b113bf6f3ae44d00ff4d9228b62
kellmant <kellman@meghu.ca>
2019-12-21 05:19:46 -0500

    module build and cleanup

--- test.js
+++ test.js
@@ -3,8 +3,6 @@
 'use strict'
 const cp = require('./fun/cp')
 const cpdata = require('./fun/cpdata')
-var limit = '500'
-var details = 'full'
 
 /**
  * Variable required from auth/mycpauth.json
@@ -25,34 +23,7 @@ main()
 async function main() {
 	cp.startSession(mycred)
         .then(() => cpdata.testcmd(mycmd))
-        //.then(myobjs => cp.writeJson(myobjs, 'dump'))
 	.then(() => cp.endSession())
 	.then(exitstat => console.log(exitstat))
 	.catch(cp.endSession)
 }
-/*
-async function testcmd() {
-    try {                
-        let mydata = {}
-	var objdata = {}
-	var objarr = []
-        //mydata['details-level'] = details
-        //mydata.limit = limit
-        console.log('testing command ' + mycmd)
-        objdata = await cp.apicall(mydata, mycmd)
-        console.log(Object.keys(objdata))
-        objarr = objarr.concat(objdata)
-        if (objdata.total > objdata.to) {
-                while (objdata.total > mydata.offset) {
-                        console.log('Indexed from ' + objdata.from + ' to ' + objdata.to + ' of ' + objdata.total + ' total objects')
-                        mydata.offset = Number(objdata.to)
-                        objdata = await cp.apicall(mydata, mycmd)
-                        objarr = objarr.concat(objdata)
-                }
-        }
-        return objarr
-    } catch (err) {
-        console.log('error in testcmd : ' + err)
-    }
-}
-*/
