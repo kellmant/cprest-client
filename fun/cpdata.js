5a9ee4cc723b64d36ad6949cab95a32b7e5e096f
kellmant <kellman@meghu.ca>
2019-12-21 23:15:19 -0500

    module build and cleanup

--- fun/cpdata.js
+++ fun/cpdata.js
@@ -201,7 +201,7 @@ async function testcmd(newcmd, details, data) {
         console.log(typeof objdata[objret][0])
         console.log(' ')
         if (typeof objdata[objret][0] === 'string') {
-            console.log(Object.keys(objdata[objret]).length)
+            console.log(Object.keys(objdata[objret]).length + ' uids returned')
         } else {
             console.log(JSON.stringify(Object.keys(objdata[objret][0])))
         }
