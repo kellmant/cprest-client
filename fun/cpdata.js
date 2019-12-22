46007dc3b861521f70f9c00c1271593de9b662eb
kellmant <kellman@meghu.ca>
2019-12-21 21:48:58 -0500

    module build and cleanup

--- fun/cpdata.js
+++ fun/cpdata.js
@@ -185,6 +185,7 @@ async function testcmd(newcmd) {
             if (objdata[obj].length > 1)
                 objret = obj
         });
+        console.log(Object.keys(objdata[obj]))
         console.log('Indexed from ' + objdata.from + ' to ' + objdata.to + ' of ' + objdata.total + ' total objects')
         objarr = objarr.concat(objdata[objret])
         if (objdata.total > objdata.to) {
